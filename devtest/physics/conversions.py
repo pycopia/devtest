a3ae07dfb8e5f1cfc40d6282a9fee8e737b8fc61
Keith Dart <dart@google.com>
2018-10-22 14:15:07 -0700

    Port pycopia3 physics package to devtest.

---
+++ devtest/physics/conversions.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+
+"""General conversion functions.
+
+Convert units from one to another. Generally you can use a PhysicalQuantity
+object to do linear conversions. But that doesn't handle logarithmic units yet.
+
+All voltages are RMS values.
+"""
+
+import math
+
+
+def dBVToVolts(dbv):
+    return 10.0**(dbv / 20.0)
+
+
+def VoltsTodBV(v):
+    return 20.0 * math.log10(v / 1.0)
+
+
+def VoltsTodBu(v):
+    return 20.0 * math.log10(v / 0.77459667)
+
+
+def dBuToVolts(dbu):
+    return 0.77459667 * 10.0**(dbu / 20.0)
+
+
+def dBmToWatts(dbm):
+    return 0.001 * 10.0**(dbm / 10.0)
+
+
+def WattsTodBm(w):
+    return 10.0 * math.log10(w / 0.001)
+
+# vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
