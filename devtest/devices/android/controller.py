839389ab46fd3af6a4c2230e69d67af0b62bb090
Keith Dart <dart@google.com>
2018-12-06 18:39:44 -0800

    Expose the Android properties in a simpler way. Add easier shell method.

--- devtest/devices/android/controller.py
+++ devtest/devices/android/controller.py
@@ -4,11 +4,18 @@
 
 from __future__ import generator_stop
 
+import re
+
 from devtest import devices
+from devtest.core import exceptions
 from devtest.devices.android import adb
 from devtest.devices.android import sl4a
 
 import uiautomator
+
+
+class AndroidControllerError(exceptions.ControllerError):
+    """Raised for errors from the AndroidController."""
 
 
 class AndroidController(devices.Controller):
@@ -20,7 +27,10 @@ class AndroidController(devices.Controller):
         adb: An adb.AndroidDeviceClient instance.
         uia: An uiautomator.Device instance.
         api: An sl4a.SL4AInterface instance.
+        properties: A dictionary of all Android property values (from getprop).
     """
+
+    _PROPERTY_RE = re.compile(r"\[(.*)\]: \[(.*)\]")
 
     def __init__(self, equipment):
         self._equipment = equipment
@@ -57,5 +67,46 @@ class AndroidController(devices.Controller):
         if self._adb is not None:
             self._adb.close()
             self._adb = None
+
+    @property
+    def properties(self):
+        """The Android properties."""
+        pd = {}
+        RE = AndroidController._PROPERTY_RE
+        text = self.shell(["getprop"])
+        for line in text.splitlines():
+            m = RE.search(line)
+            if m:
+                name, value = m.group(1, 2)
+                pd[name] = value
+        return pd
+
+    def shell(self, cmd):
+        """Run a shell command and return stdout.
+
+        Args:
+            cmd: list or string, command to run
+
+        Returns:
+            The stdout as str (decoded).
+
+        Raises:
+            AndroidControllerError with exit status and stderr as args.
+        """
+        stdout, stderr, es = self.adb.command(cmd)
+        if es:
+            return stdout
+        else:
+            raise AndroidControllerError((es, stdout))
+
+    def get_property(self, name):
+        """Get a single Android property.
+        """
+        return self.shell(["getprop", name]).strip()
+
+    def set_property(self, name, value):
+        """Set a single Android property.
+        """
+        return self.shell(["setprop", name, value]).strip()
 
 # vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
