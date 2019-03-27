ec96672660f6519e5617c3877831a587ffc8db3b
Keith Dart <dart@google.com>
2019-03-27 12:58:26 -0700

    Add dummy OS modules for Darwin.
    Not fully implemented, but at least importable.

---
+++ devtest/os/darwin/cpuinfo.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3.7
+
+"""CPU Information and monitors.
+"""
+
+import typing
+
+
+class ProcStat(typing.NamedTuple):
+    """Process status.
+    """
+    pid: int  # process id
+    ppid: int  # process id of the parent process
+    pgrp: int  # pgrp of the process
+
+    @classmethod
+    def from_text(cls, bytesblob):
+        res = [int(s) for s in bytesblob.split()]
+        return cls(*res)
+
+    @classmethod
+    def from_pid(cls, pid):
+        text = b'%d 0 0' % pid  # TODO
+        return cls.from_text(text)
+
+
+if __name__ == "__main__":
+    import os
+    ps = ProcStat.from_pid(os.getpid())
+    print(ps)
+#    mon = CPUUtilizationMonitor(os.getpid())
+#    mon.start()
+#    time.sleep(2.9)
+#    for i in range(10000000):
+#        x = i ** 2
+#    print(mon.current())
+#    mon.end()
+#    del mon
+
+# vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
+
---
+++ devtest/os/darwin/meminfo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3.7
+
+"""Memory info for Darwin.
+"""
+
+# TODO
+
+
+class VirtualMemoryArea:
+    pass
+
+
+class Maps(list):
+    """A list of `VirtualMemoryArea`s.
+    """
+    @classmethod
+    def from_text(cls, bytesblob):
+        pass
+
+# vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
